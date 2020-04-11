898be913589d900b70d4575e1fac9fce74fa8c67
yuto.tsuchiya <tsuchiya0410yuto@gmail.com>
2020-04-11 10:11:44 +0000

    addtask

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -50,7 +50,7 @@ class TasksController < ApplicationController
   private
   
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
 
 end
