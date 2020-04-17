90efdd5b340ab8676d72f2c927c8c882693ee86f
yuto.tsuchiya <tsuchiya0410yuto@gmail.com>
2020-04-17 12:09:30 +0000

    loginandtask

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,22 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  
   def index
-    @tasks = Task.all
+    if logged_in?
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    end
   end
   
   def show
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
   end
   
   def new
-    @task = Task.new
+    @task = current_user.tasks.new
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'taskが正常に投稿されました'
@@ -24,11 +28,11 @@ class TasksController < ApplicationController
   end
   
   def edit
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
   end
   
   def update
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
 
     if @task.update(task_params)
       flash[:success] = 'Message は正常に更新されました'
@@ -40,7 +44,7 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
     @task.destroy
     
     flash[:success] = 'taskは正常に削除されました'
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,11 @@ Rails.application.routes.draw do
   
   resources :tasks
   
-  post 'tasks/new', to: 'tasks#create'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :new, :create]
+  
 end
